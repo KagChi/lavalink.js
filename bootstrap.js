749ad05d471a72aff428d9cf3c0c666e0d116ba1
darkforest-beep <59391215+darkforest-beep@users.noreply.github.com>
2020-06-04 11:36:38 +0700

    Update bootstrap.js

--- bootstrap.js
+++ bootstrap.js
@@ -51,5 +51,5 @@ function startLavalink() {
     });
 }
 
-const cdn = 'http://cdn.glitch.com/77b0e0bb-e744-442a-bbbd-03c3fe832534%2FLavalink.jar?v=1587249367105'
+const cdn = 'https://cdn.glitch.com/2cf2d9f7-ec3a-4513-a642-80d41b6148fe%2FLavalink.jar?v=1591244803929'
 download(cdn, './Lavalink.jar', startLavalink)
